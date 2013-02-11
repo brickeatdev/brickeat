b31c7fd463b9f3985120faa7543b528570b2fbc3
ArseneSydrie <arsene.mabom@gmail.com>
2013-02-11 17:38:52 +0100

    MAJ WIDTH & HEIGHT

--- BrickEat/src/com/brickeat/util/constants/BrickeatConstants.java
+++ BrickEat/src/com/brickeat/util/constants/BrickeatConstants.java
@@ -15,9 +15,9 @@ public interface BrickeatConstants {
 
 	public static final int FPS = 30;
 
-	public static final int GAME_WIDTH = 720;
+	public static final int GAME_WIDTH = 480;
 	public static final int GAME_WIDTH_HALF = GAME_WIDTH / 2;
-	public static final int GAME_HEIGHT = 480;
+	public static final int GAME_HEIGHT = 720;
 	public static final int GAME_HEIGHT_HALF = GAME_HEIGHT / 2;
 
 	public static final int PADDLE_WIDTH = 20;
